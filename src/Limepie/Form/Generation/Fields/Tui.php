f5f8f9a0130b416cd523183d80fd1c09e76a2f5e
yejune <k@yejune.com>
2020-03-01 09:37:59 +0900

    cleanup tui

--- src/Limepie/Form/Generation/Fields/Tui.php
+++ src/Limepie/Form/Generation/Fields/Tui.php
@@ -14,17 +14,24 @@ class Tui extends \Limepie\Form\Generation\Fields
         $default = $property['default'] ?? '';
         $rows    = $property['rows']    ?? 5;
 
-        $upload = $property['upload'] ?? 'upload';
+        $fileserver = $property['fileserver'] ?? '';
+        $class = $property['class'] ?? '';
+        $linkcss = $property['linkcss'] ?? '';
+        if($linkcss) {
+            $linkcss = '<link rel="stylesheet" href="'.$linkcss.'"></link>';
+        }
         $id = uniqid();
         $html = <<<EOT
         <link rel="stylesheet" href="https://uicdn.toast.com/tui-editor/latest/tui-editor.css"></link>
         <link rel="stylesheet" href="https://uicdn.toast.com/tui-editor/latest/tui-editor-contents.css"></link>
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/codemirror/5.48.4/codemirror.css"></link>
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/highlight.js/9.12.0/styles/github.min.css"></link>
+        {$linkcss}
         <script src="https://uicdn.toast.com/tui-editor/latest/tui-editor-Editor-full.js"></script>
-        <textarea class="form-control d-none" id="textarea{$id}" name="{$key}" data-default="{$default}" rows="{$rows}">{$value}</textarea>
+        <textarea class="form-control d-none" id="textarea{$id}" name="{$key}">{$value}</textarea>
+        <textarea class="form-control d-none" id="textarea{$id}_html" name="{$key}_html"></textarea>
 
-        <div id="tui{$id}" class="form-control"  style="display:block; width: 100%" data-default="{$default}" rows="{$rows}"></div>
+        <div id="tui{$id}" class="form-control {$class}"  style="display:block; width: 100%"></div>
         <style>#tui{$id} .te-mode-switch-section {
          display: none !important;
          height: 0;
@@ -36,12 +43,34 @@ $(function() {
         el: document.querySelector('#tui{$id}'),
         previewStyle: 'vertical',
         width: '100%',
-        height: '300px',
+        height: 'auto',
         initialEditType: 'markdown',
         initialValue: $('#textarea{$id}').val(),
         events: {
             change: function() {
                 $('#textarea{$id}').val(editor.getMarkdown())
+                $('#textarea{$id}_html').val(editor.getHtml())
+            }
+        },
+        hooks: {
+            'addImageBlobHook': function(blob, callback) {
+                var formData = new FormData();
+                formData.append('image', blob);
+
+                $.ajax({
+                    url: "{$fileserver}",
+                    enctype: 'multipart/form-data',
+                    data: formData,
+                    processData: false,
+                    contentType: false,
+                    cache: false,
+                    type: 'POST',
+                    success: function(response){
+                        callback(response.data.url, '');
+                    },
+                    error: function(e) {
+                    }
+                });
             }
         },
         exts: ['scrollSync']
